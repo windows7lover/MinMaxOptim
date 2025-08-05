e0b8cf1e146add543303c56708f60ce729cd8e21
windows7lover <damien.scieur@gmail.com>
2025-08-05 14:16:53 -0400

    Implemented SimGDA

--- Matlab/main.m
+++ Matlab/main.m
@@ -18,12 +18,14 @@ finfo = randomQuadratic(dim_x, dim_y, L_x, L_y, mu_x, mu_y, L_xy);
 % Algo Parameter
 nIter = 10000;
 
-param_GDA = [];
+param_altGDA = [];
+param_simGDA = [];
 
 
 % Concatenate algo for experiment
 algoCell = {...
-    @(x,y,z) alternatingDescentAscent(x,y,z), param_GDA
+    @(x,y,z) alternatingDescentAscent(x,y,z), param_altGDA;
+    @(x,y,z) simDescentAscent(x,y,z), param_simGDA
     };
 numExp = size(algoCell,1);
 
@@ -31,21 +33,28 @@ algoResult = cell(numExp,1);
 finalIterate = cell(numExp,2);
 
 % Run the experiment
-for exp=1:size(numExp,1)
+for exp=1:numExp
     [algoResult{exp}, finalIterate{exp,1}, finalIterate{exp,2}] = algoCell{exp,1}(finfo, nIter, algoCell{exp,2});
 end
 
 % Plot the experiment
 fstar = finfo.fstar;
 
-figure
+labelCell = cell(numExp,1);
 for exp=1:numExp
-    semilogy(algoResult{exp}.iterVec, algoResult{exp}.fval-fstar)
-    hold on
+    labelCell{exp} = algoResult{exp}.name;
 end
+
+% figure
+% for exp=1:numExp
+%     semilogy(algoResult{exp}.iterVec, algoResult{exp}.fval-fstar)
+%     hold on
+% end
+% legend(labelCell)
 
 figure
 for exp=1:numExp
     semilogy(algoResult{exp}.iterVec, algoResult{exp}.gradval_x + algoResult{exp}.gradval_y)
     hold on
 end
+legend(labelCell)
